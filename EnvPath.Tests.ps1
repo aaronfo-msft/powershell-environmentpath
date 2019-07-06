c8b49e8403536b9225d15c45e0f397d8368a0bb5
Aaron Fortner <45862009+aaronfo-msft@users.noreply.github.com>
2019-07-06 00:43:24 -0700

    Add some Get tests

---
+++ EnvPath.Tests.ps1
@@ -0,0 +1,43 @@
+using namespace System;
+
+Remove-Module EnvPath
+Import-Module .\EnvPath.psm1 -Force
+
+Describe "Get-EnvPath tests" {
+    Context "Implicit variable does not exist in any scope" {
+        Mock -ModuleName EnvPath getEnvironmentVariable -MockWith { "" }
+        It "Throws" {
+            { Get-EnvPath } | Should Throw "Exception of type 'VariableNotFoundException' was thrown."
+        }
+    }
+    Context "Implicit variable exists at Machine scope only" {
+        Mock -ModuleName EnvPath getEnvironmentVariable -MockWith { 
+            Param ($Variable, $Scope)
+            if ($Scope -eq [EnvironmentVariableTarget]::Machine) {
+                return "C:\"
+            }
+        }
+        $actual = Get-EnvPath
+        It "Path is correct" {
+            $actual.Path | Should Be "C:\"
+        }
+        It "Scope is correct" {
+            $actual.Scope | Should Be Machine
+        }
+    }
+    Context "Implicit variable exists at User scope only" {
+        Mock -ModuleName EnvPath getEnvironmentVariable -MockWith { 
+            Param ($Variable, $Scope)
+            if ($Scope -eq [EnvironmentVariableTarget]::User) {
+                return "C:\"
+            }
+        }
+        $actual = Get-EnvPath
+        It "Path is correct" {
+            $actual.Path | Should Be "C:\"
+        }
+        It "Scope is correct" {
+            $actual.Scope | Should Be User
+        }
+    }
+}
